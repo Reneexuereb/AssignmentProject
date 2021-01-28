56364244e5f23d7fe7612b1d4b3a3fe63b99356c
Cpr95 <74544949+Cpr95@users.noreply.github.com>
2021-01-28 12:41:53 +0100

    Arranged score layout

---
+++ SubwaySurfer/Assets/Scripts/Obstacles/ObstacleCollisionHandler.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ObstacleCollisionHandler : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("The player has entered obstacle mode");
+        //reload the scene when the player hits the obstacles
+        SceneManager.LoadScene(0);
+    }
+}
